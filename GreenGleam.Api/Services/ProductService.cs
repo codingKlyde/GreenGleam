306635fa1d7ec38f8dc2c5ca5f02f356ca52c0be
Klyde Bairulla <klydefb@gmail.com>
2025-08-03 07:15:56 +0800

    Add project file

---
+++ GreenGleam.Api/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿namespace GreenGleam.Api.Services
+{
+    public class ProductService
+    {
+        private readonly DataContext _dataContext;
+
+        public  ProductService (DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<ProductDto[]> GetProductsAsync() => await _dataContext.Products
+            .AsNoTracking()
+            .Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Image = p.Image,
+                Description = p.Description,
+                Price = p.Price,
+                Unit = p.Unit
+            })
+            .ToArrayAsync();
+    }
+}

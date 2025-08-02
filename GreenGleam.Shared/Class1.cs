7a8cb372b536344915770681592f7a1e1f3c5f3c
Klyde Bairulla <klydefb@gmail.com>
2025-08-02 13:49:16 +0800

    Add project files.

---
+++ GreenGleam.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace GreenGleam.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ GreenGleam/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GreenGleam
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

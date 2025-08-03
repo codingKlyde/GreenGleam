3d21d78485528ac422ecbd67074ff4fb8d39f604
Klyde Bairulla <klydefb@gmail.com>
2025-08-03 20:13:33 +0800

    Add project file

---
+++ GreenGleam.Shared/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace GreenGleam.Shared
+{
+    public static class Extensions
+    {
+        public static int GetUserId(this ClaimsPrincipal claimsPrincipal) => Convert.ToInt32(claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+    }
+}

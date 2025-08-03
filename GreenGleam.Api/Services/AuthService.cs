52c64a54cb4a5b4d3010fa649983ec44034118d5
Klyde Bairulla <klydefb@gmail.com>
2025-08-03 08:51:59 +0800

    Add project file

---
+++ GreenGleam.Api/Services/AuthService.cs
@@ -0,0 +1,56 @@
+﻿namespace GreenGleam.Api.Services
+{
+    public class AuthService
+    {
+        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly DataContext _dataContext;
+
+        public AuthService(IPasswordHasher<User> passwordHasher, DataContext dataContext)
+        {
+            _passwordHasher = passwordHasher;
+            _dataContext = dataContext;
+        }
+
+        public async Task<ApiResultDto> RegisterAsync(RegisterDto registerDto)
+        {
+            if (await _dataContext.Users.AnyAsync(u => u.Email == registerDto.Email))
+                return ApiResultDto.Fail("Email already exists");
+
+            var user = new User
+            {
+                Name = registerDto.Name,
+                Email = registerDto.Email,
+                MobileNumber = registerDto.MobileNumber
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, registerDto.Password);
+
+            try
+            {
+                _dataContext.Users.Add(user);
+                await _dataContext.SaveChangesAsync();
+                return ApiResultDto.Success();
+            }
+            catch (Exception ex)
+            {
+                return ApiResultDto.Fail(ex.Message);
+            }
+        }
+
+        public async Task<ApiResultDto<LoggedInUserDto>> LoginAsync(LoginDto loginDto)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.Email == loginDto.Email);
+            if (user is null)
+                return ApiResultDto<LoggedInUserDto>.Fail("User does not exist");
+
+            var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, loginDto.Password);
+            if (result != PasswordVerificationResult.Success)
+                return ApiResultDto<LoggedInUserDto>.Fail("Incorrect password");
+
+            var jwt = "";
+            var loggedInUser = new LoggedInUserDto(user.Id, user.Name, user.Email, jwt);
+
+            return ApiResultDto<LoggedInUserDto>.Success(loggedInUser);
+        } 
+    }
+}
---
+++ GreenGleam.Api/Services/UserService.cs
@@ -0,0 +1,72 @@
+﻿namespace GreenGleam.Api.Services
+{
+    public class UserService
+    {
+        private readonly DataContext _dataContext;
+
+        public UserService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<ApiResultDto> SaveAddressAsync(AddressDto addressDto, int userId)
+        {
+            UserAddress? userAddress = null;
+            if (addressDto.Id == 0)
+            {
+                userAddress = new UserAddress
+                {
+                    Id = addressDto.Id,
+                    Name = addressDto.Name,
+                    Address = addressDto.Address,
+                    IsDefault = addressDto.isDefault,
+
+                    UserId = userId
+                };
+
+                _dataContext.UserAddresses.Add(userAddress);
+            }
+            else
+            {
+                userAddress = await _dataContext.UserAddresses.FindAsync(addressDto.Id);
+                if (userAddress is null)
+                    return ApiResultDto.Fail("Invalid request");
+
+                userAddress.Name = addressDto.Name;
+                userAddress.Address = addressDto.Address;
+                userAddress.IsDefault = addressDto.isDefault;
+
+                _dataContext.UserAddresses.Update(userAddress);
+            }
+
+            try
+            {
+                if (addressDto.isDefault)
+                {
+                    var defaultAddress = await _dataContext.UserAddresses.FirstOrDefaultAsync(d => d.UserId == userId && d.IsDefault && d.Id != addressDto.Id);
+                    if (defaultAddress is not null)
+                        defaultAddress.IsDefault = false;
+                }
+
+                await _dataContext.SaveChangesAsync();
+                return ApiResultDto.Success();
+            }
+            catch (Exception ex)
+            {
+                return ApiResultDto.Fail(ex.Message);
+            }
+        }
+
+        public async Task<AddressDto[]> GetAddresses(int userId) => await _dataContext.UserAddresses
+            .AsTracking()
+            .Where(a => a.Id == userId)
+            .Select(a => new AddressDto
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Address = a.Address,
+                isDefault = a.IsDefault
+            })
+            .ToArrayAsync();
+    }
+}

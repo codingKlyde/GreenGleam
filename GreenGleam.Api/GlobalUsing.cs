ac1c73e797e589f27834cbc16083ae2f9f781ab2
Klyde Bairulla <klydefb@gmail.com>
2025-08-03 20:14:25 +0800

    Update GlobalUsing

--- GreenGleam.Api/GlobalUsing.cs
+++ GreenGleam.Api/GlobalUsing.cs
@@ -1,10 +1,13 @@
 ﻿global using GreenGleam.Api.Data;
 global using GreenGleam.Api.Data.Entities;
+global using GreenGleam.Api.Services;
 global using GreenGleam.Shared.Contants;
+global using GreenGleam.Shared;
 global using GreenGleam.Shared.Dtos;
 
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Security.Claims;
 
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
--- GreenGleam.Shared/GlobalUsing.cs
+++ GreenGleam.Shared/GlobalUsing.cs
@@ -1,3 +1,4 @@
 ﻿global using GreenGleam.Shared.Contants;
 
 global using System.ComponentModel.DataAnnotations;
+global using System.Security.Claims;

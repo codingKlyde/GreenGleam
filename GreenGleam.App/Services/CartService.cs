442e987babfa203a1afddb380fb36758f39d0da6
Klyde Bairulla <klydefb@gmail.com>
2025-08-13 16:48:05 +0800

    Add project file

---
+++ GreenGleam.App/Services/CartService.cs
@@ -0,0 +1,23 @@
+﻿namespace GreenGleam.App.Services
+{
+    public class CartService
+    {
+        public List<ProductDto> CartItems { get; private set; } = [];
+        public int Count { get; private set; }
+        public string CountDisplay => Count < 100 ? $"{Count}" : "99";
+
+        public event Action? CartItemCountChanged;
+
+        public void AddToCart(ProductDto product)
+        {
+            Count++;
+            CartItemCountChanged?.Invoke();
+        }
+
+        public void RemoveFromCart(ProductDto product)
+        {
+            Count--;
+            CartItemCountChanged?.Invoke();
+        }
+    }
+}

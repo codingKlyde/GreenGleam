d208173d45db156056224581c4d226f295fe8c1f
Klyde Bairulla <klydefb@gmail.com>
2025-08-15 14:31:59 +0800

    Get details from ProductDto

--- GreenGleam.App/Data/CartModel.cs
+++ GreenGleam.App/Data/CartModel.cs
@@ -4,17 +4,15 @@
     public class CartModel
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public string Image { get; set; }
-
         public string Description { get; set; }
-
         public decimal Price { get; set; }
-
         public string Unit { get; set; }
         public int Quantity { get; set; }
+
+        public int ProductId { get; set; }
+
 
         public static CartModel FromDto(ProductDto productDto)
         {
@@ -25,7 +23,9 @@
                 Description = productDto.Description,
                 Price = productDto.Price,
                 Unit = productDto.Unit,
-                Quantity = productDto.Quantity
+                Quantity = productDto.Quantity,
+
+                ProductId = productDto.Id
             };
         }
     }

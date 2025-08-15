4dd9bfb3df0b151a84cf8a93dee2db042fb9c736
Klyde Bairulla <klydefb@gmail.com>
2025-08-15 14:20:15 +0800

    Add project file

---
+++ GreenGleam.App/Data/CartModel.cs
@@ -0,0 +1,32 @@
+﻿namespace GreenGleam.App.Data
+{
+    // It is used for the cart functionality in the app
+    public class CartModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Unit { get; set; }
+        public int Quantity { get; set; }
+
+        public static CartModel FromDto(ProductDto productDto)
+        {
+            return new CartModel
+            {
+                Name = productDto.Name,
+                Image = productDto.Image,
+                Description = productDto.Description,
+                Price = productDto.Price,
+                Unit = productDto.Unit,
+                Quantity = productDto.Quantity
+            };
+        }
+    }
+}

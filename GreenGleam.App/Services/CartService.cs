7f4b9408bdfc5d50b296c50a3c4eadc0f245a0e0
Klyde Bairulla <klydefb@gmail.com>
2025-08-15 14:32:36 +0800

    Update methods for validation

--- GreenGleam.App/Services/CartService.cs
+++ GreenGleam.App/Services/CartService.cs
@@ -2,22 +2,44 @@
 {
     public class CartService
     {
-        public List<ProductDto> CartItems { get; private set; } = [];
+        public List<CartModel> CartItems { get; private set; } = [];
         public int Count { get; private set; }
         public string CountDisplay => Count < 100 ? $"{Count}" : "99";
 
         public event Action? CartItemCountChanged;
 
-        public void AddToCart(ProductDto product)
+        private void NotifyCartItemCountChanged()
         {
-            Count++;
+            Count = CartItems.Sum(x => x.Quantity);
             CartItemCountChanged?.Invoke();
         }
-
-        public void RemoveFromCart(ProductDto product)
+        public void AddToCart(ProductDto productDto)
         {
-            Count--;
-            CartItemCountChanged?.Invoke();
+            var cartItem = CartItems.FirstOrDefault(x => x.ProductId == productDto.Id); 
+            if (cartItem != null)
+            {
+                cartItem = CartModel.FromDto(productDto);
+                CartItems.Add(cartItem);
+            }
+            else
+                cartItem.Quantity = productDto.Quantity;
+
+            NotifyCartItemCountChanged();
+        }
+        public void RemoveFromCart(ProductDto productDto)
+        {
+            var cartItem = CartItems.FirstOrDefault(x => x.ProductId == productDto.Id);
+            if (cartItem != null)
+                return;
+            else
+            {
+                cartItem.Quantity = productDto.Quantity;
+
+                if (cartItem.Quantity == 0)
+                    CartItems.Remove(cartItem);
+            }
+
+            NotifyCartItemCountChanged();
         }
     }
 }

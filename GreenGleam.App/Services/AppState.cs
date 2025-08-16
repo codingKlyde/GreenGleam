b7223df350d6378a253ad1f9a59ab08b9202a662
Klyde Bairulla <klydefb@gmail.com>
2025-08-16 22:51:26 +0800

    Add project file

---
+++ GreenGleam.App/Services/AppState.cs
@@ -0,0 +1,11 @@
+﻿namespace GreenGleam.App.Services
+{
+    public partial class AppState : ObservableObject
+    {
+        [ObservableProperty][NotifyPropertyChangedFor(nameof(IsNotBusy))] bool isBusy;
+        public bool IsNotBusy => !IsBusy;
+
+        public void ShowLoader() => IsBusy = true;
+        public void HideLoader() => IsBusy = false;
+    }
+}
---
+++ GreenGleam.App/Services/MauiInterop.cs
@@ -0,0 +1,11 @@
+﻿namespace GreenGleam.App.Services
+{
+    public static class MauiInterop
+    {
+        public static async Task ToastAsync(string message) => await Toast.Make(message).Show();
+
+        public static async Task AlertAsync(string message, string title = "Alert") => await App.Current.Windows[0].Page.DisplayAlert(title, message, "OK");
+
+        public static async Task<bool> ConfirmAsync(string message, string title = "Confirm") => await App.Current.Windows[0].Page.DisplayAlert(title, message, "Yes", "No");
+    }
+}

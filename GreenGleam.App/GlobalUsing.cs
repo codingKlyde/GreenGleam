349a5c346d2ca2780476e99554c45de1066c5862
Klyde Bairulla <klydefb@gmail.com>
2025-08-20 11:53:29 +0800

    Update GlobalUsing

--- GreenGleam.App/GlobalUsing.cs
+++ GreenGleam.App/GlobalUsing.cs
@@ -3,6 +3,11 @@ global using GreenGleam.App.Data;
 global using GreenGleam.App.Services;
 global using GreenGleam.Shared.Dtos;
 
+global using System.Text.Json;
+
 global using Microsoft.Extensions.Logging;
+
+global using CommunityToolkit.Maui.Alerts;
+global using CommunityToolkit.Mvvm.ComponentModel;
 
 global using Refit;

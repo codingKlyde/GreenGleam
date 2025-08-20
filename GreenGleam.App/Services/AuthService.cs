70733260d2fda2066a812dbd159f49ecdfb2ed11
Klyde Bairulla <klydefb@gmail.com>
2025-08-20 11:34:21 +0800

    Add project file

---
+++ GreenGleam.App/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿namespace GreenGleam.App.Services
+{
+    public class AuthService
+    {
+        private const string UserDataKey = "user-data";
+        public LoggedInUserDto? LoggedInUser {  get; set; }
+        public bool IsLoggedIn { get; private set; }
+
+        public AuthService()
+        {
+            var userData = StorageService.GetFromStorage<LoggedInUserDto?>(UserDataKey, null);
+
+            if (userData != null)
+            {
+                LoggedInUser = userData;
+                IsLoggedIn = true;
+            }
+        }
+
+        public void Login(LoggedInUserDto loggedInUserDto)
+        {
+            StorageService.SaveToStorage(UserDataKey, loggedInUserDto);
+            LoggedInUser = loggedInUserDto;
+            IsLoggedIn = true;
+        }
+        public void Logout()
+        {
+            LoggedInUser = null;
+            IsLoggedIn = false;
+            StorageService.RemoveFromStorage(UserDataKey);
+        }
+    }
+}

f6929d79e865c05735c98e16587fd5be68592a8c
Klyde Bairulla <klydefb@gmail.com>
2025-08-20 11:49:44 +0800

    Add project file

---
+++ GreenGleam.App/Services/StorageService.cs
@@ -0,0 +1,26 @@
+﻿namespace GreenGleam.App.Services
+{
+    public class StorageService
+    {
+        public static void SaveToStorage<Tvalue>(string key, Tvalue tvalue)
+        {
+            var serializedValue = JsonSerializer.Serialize(tvalue);
+            Preferences.Default.Set(key, serializedValue);
+        }
+        public static TValue GetFromStorage<TValue>(string key, TValue tvalue)
+        {
+            if (Preferences.Default.ContainsKey(key))
+            {
+                var serializedValue = Preferences.Default.Get<string?>(key, null);
+
+                if (!string.IsNullOrWhiteSpace(serializedValue))
+                {
+                    return JsonSerializer.Deserialize<TValue>(serializedValue)!;
+                }
+            }
+
+            return tvalue;
+        }
+        public static void RemoveFromStorage(string key) => Preferences.Default.Remove(key);
+    }
+}

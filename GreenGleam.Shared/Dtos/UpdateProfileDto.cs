055a6bd94a73c6c76e3b54287f68cab0b4de58d0
Klyde Bairulla <klydefb@gmail.com>
2025-08-31 22:19:31 +0800

    Add project file

---
+++ GreenGleam.Shared/Dtos/UpdateProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GreenGleam.Shared.Dtos
+{
+    public class UpdateProfileDto
+    {
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string? MobileNumber { get; set; }
+    }
+}

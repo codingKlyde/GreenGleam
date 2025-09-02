345c7a6c9e0e590399f3054980bafb75e4ce2af5
Klyde Bairulla <klydefb@gmail.com>
2025-09-02 11:31:37 +0800

    Update migration

--- GreenGleam.Api/Data/Migrations/20250902025743_FixedTypo.Designer.cs
+++ GreenGleam.Api/Data/Migrations/20250902025743_FixedTypo.Designer.cs
@@ -4,6 +4,7 @@ using GreenGleam.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GreenGleam.Api.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250902025743_FixedTypo")]
+    partial class FixedTypo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -42,6 +45,7 @@ namespace GreenGleam.Api.Data.Migrations
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Notes")
+                        .IsRequired()
                         .HasMaxLength(500)
                         .HasColumnType("nvarchar(500)");
 
---
+++ GreenGleam.Api/Data/Migrations/20250902025743_FixedTypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenGleam.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProducImage",
+                table: "OrderItems",
+                newName: "ProductImage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductImage",
+                table: "OrderItems",
+                newName: "ProducImage");
+        }
+    }
+}

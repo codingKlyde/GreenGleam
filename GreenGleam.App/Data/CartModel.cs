a19d90d0a2b43b8735675c81683c81c214dbd078
Klyde Bairulla <klydefb@gmail.com>
2025-09-03 14:19:01 +0800

    Update property name

--- GreenGleam.App/Data/CartModel.cs
+++ GreenGleam.App/Data/CartModel.cs
@@ -1,19 +1,19 @@
 ﻿namespace GreenGleam.App.Data
 {
-    // It is used for the cart functionality in the app
     public class CartModel
     {
-        public int Id { get; set; }
+        [PrimaryKey, AutoIncrement]
+        public int CartItemId { get; set; }
+        public int ProductId { get; set; }
+
         public string Name { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Unit { get; set; }
         public int Quantity { get; set; }
+        [Ignore]
         public decimal Total => Quantity * Price;
-
-        public int ProductId { get; set; }
-
 
         public static CartModel FromDto(ProductDto productDto)
         {
